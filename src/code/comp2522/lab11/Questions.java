dc7e1cf6a6663177d54a0dce21cb95d81cc8d550
haileykim03 <skim682@my.bcit.ca>
2025-12-01 14:29:30 -0800

    javadoc for Question and Questions

--- src/code/comp2522/lab11/Questions.java
+++ src/code/comp2522/lab11/Questions.java
@@ -9,7 +9,11 @@ import java.util.ArrayList;
 import java.util.Random;
 
 /**
- * Reads from a file of questions, can return those questions as question and answers
+ * Reads from a file of questions, can return those questions as question and answers.
+ *
+ * @author Hailey Kim
+ * @author Luis Seberon
+ * @version 1.0
  */
 public class Questions
 {
@@ -19,21 +23,35 @@ public class Questions
     private static final int            QUESTION  = 0;
     private static final int            ANSWER    = 1;
     private static final Random         rand      = new Random();
-    private final        List<Question> questions;
 
+    private final List<Question>        questions;
+
+
+    /**
+     * Constructs a Questions object.
+     */
     public Questions()
     {
         this.questions = readQuestionsFromFile();
     }
 
+
+    /**
+     * Reads lines from the source file and parses them into Question objects.
+     *
+     * @return a List of Question objects parsed from the file
+     */
     private List<Question> readQuestionsFromFile()
     {
         final List<Question>    questions;
         final List<String>      lines;
+
         questions = new ArrayList<>();
-        try{
+
+        try
+        {
             lines =  Files.readAllLines(PATH);
-            for(String line : lines)
+            for (final String line : lines)
             {
                 final String[] splitLine;
                 final Question question;
@@ -42,7 +60,8 @@ public class Questions
                 question = new Question(splitLine[QUESTION], splitLine[ANSWER]);
                 questions.add(question);
             }
-        } catch (final IOException e)
+        }
+        catch (final IOException e)
         {
             e.printStackTrace();
         }
@@ -50,6 +69,12 @@ public class Questions
         return questions;
     }
 
+
+    /**
+     * Retrieves a random question from the list.
+     *
+     * @return a randomly selected Question object
+     */
     public Question getRandomQuestion()
     {
         final int  index;
